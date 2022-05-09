01ceea328a604bbb61240c1bc8a1f48b179d7feb
Marco <marco.klauenboesch@qualiflex.solutions>
2022-05-09 16:29:42 +0200

    small css class change, added test chart

--- web/src/pages/ChartsPage/ChartsPage.tsx
+++ web/src/pages/ChartsPage/ChartsPage.tsx
@@ -1,19 +1,65 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts'
+
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+]
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+
+const RADIAN = Math.PI / 180
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+  const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  )
+}
 const ChartsPage = () => {
   return (
     <>
       <MetaTags title="Charts" description="Charts page" />
 
       <h1>ChartsPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ChartsPage/ChartsPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>charts</code>, link to me with `
-        <Link to={routes.charts()}>Charts</Link>`
-      </p>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
     </>
   )
 }
